223304cd4b6b7ba33dd106ce48e3e14b59e66c07
lisu60 <lisu60g@gmail.com>
2018-09-15 18:12:37 +0800

    Add report endpoint

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Report(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=str)
+    parser.add_argument('state', type=dict)
 
+    def post(self):
+        args = self.parser.parse_args()
+        print("ID: %s" % args["id"])
+        print("State: ")
+        print(args["state"])
+        return 200
+
+
+api.add_resource(Report, '/report')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
